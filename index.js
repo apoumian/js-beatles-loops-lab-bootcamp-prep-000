9c3c0b050be2d0a04d2e363cecad4c16bc138592
Arturo Poumian <apoumians@gmail.com>
2018-03-31 03:48:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,8 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var array = [];
   var i = 0;
-  while
+  while(i < facts.length) {
+    array.push(facts[i] + "!!!");
+    i++
+  }
 }
